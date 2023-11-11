09fade51e4761eac1bd5a9499e4e2fea03fddb85
Fangyany <thesecondunknow@163.com>
2023-11-11 14:03:01 +0800

    first commit

--- preprocess_traj.py
+++ preprocess_traj.py
@@ -4,6 +4,12 @@ import pickle
 import numpy as np
 from tqdm import tqdm
 
+import os
+import pandas as pd
+import pickle
+import numpy as np
+from tqdm import tqdm
+
 
 def read_and_concatenate_files(folder_path):
     trajectory_data = pd.DataFrame()
@@ -18,30 +24,29 @@ def read_and_concatenate_files(folder_path):
 
 
 
-def filter_nearby_vehicles(target_vehicle, grouped):
-    x_target, y_target = target_vehicle['x'], target_vehicle['y']
-    target_others_info = []
+def filter_nearby_vehicles(target_id, target_vehicle, grouped):
+    target_area_vehicles = pd.DataFrame()
 
     for timestamp, group in grouped:
-        # 排除目标车辆，计算其他车辆到目标车辆的距离
-        target_id = target_vehicle['vehicle_id']
-        others = group[group['vehicle_id'] != target_id]
-        others = others.assign(distance=np.sqrt((others['x'] - x_target) ** 2 + (others['y'] - y_target) ** 2))
-        nearby_vehicles = others[others['distance'] < 30]
+        # 将目标车辆的信息加入 target_area_vehicles
+        target_info = target_vehicle[target_vehicle['timestamp'] == timestamp]
+        target_area_vehicles = target_area_vehicles.append(target_info, ignore_index=True)
 
-        # 将目标车辆和周围车辆的信息加入target_others_info
-        target_info = [int(target_vehicle['vehicle_id']), int(timestamp), float(x_target), float(y_target)]
-        target_others_info.append(target_info)
+        # 确保目标信息非空
+        if not target_info.empty:
+            x_target, y_target = target_info['x'].values[0], target_info['y'].values[0]
 
-        other_vehicles_info = nearby_vehicles[['vehicle_id', 'timestamp', 'x', 'y']].values.tolist()
-        target_others_info.extend(other_vehicles_info)
+            # 计算每行的距离
+            group['distance_to_target'] = np.sqrt((group['x'] - x_target) ** 2 + (group['y'] - y_target) ** 2)
 
-    target_others_info_df = pd.DataFrame(target_others_info, columns=['vehicle_id', 'timestamp', 'x', 'y'])
-    target_others_info_df['timestamp'] = target_others_info_df['timestamp'].astype(int)
-    return target_others_info_df
+            # 筛选条件：vehicle_id不等于target_id且距离小于30
+            filtered_group = group[(group['vehicle_id'] != target_id) & (group['distance_to_target'] < 30)]
+            target_area_vehicles = target_area_vehicles.append(filtered_group[['vehicle_id', 'timestamp', 'x', 'y']], ignore_index=True)
+    return target_area_vehicles
 
 
-def process_vehicle_data(target_id, filtered_df, all_timestamps):
+
+def process_vehicle_data(target_id, target_group, filtered_df, all_timestamps):
     filtered_df['frame_exists'] = 1
     grouped_by_vehicle = filtered_df.groupby('vehicle_id')
 
@@ -64,7 +69,6 @@ def process_vehicle_data(target_id, filtered_df, all_timestamps):
     processed_data.insert(0, merged_target_data)
 
     return processed_data
-
 
 
 
@@ -98,34 +102,33 @@ def construct_features_and_labels(sliced_data, traj_data):
     return traj_data
 
 
+if __name__ == '__main__':
+    folder_path = 'trajectories'
+    traj_data = []
+    all_timestamps = range(600)
+    for root, dirs, files in os.walk(folder_path):
+        for folder in dirs:
+            folder_path = os.path.join(root, folder)
+            print(f"当前文件夹: {folder_path}")
 
-folder_path = 'trajectories'
-traj_data = []
-all_timestamps = range(600)
-for root, dirs, files in os.walk(folder_path):
-    for folder in dirs:
-        folder_path = os.path.join(root, folder)
-        print(f"当前文件夹: {folder_path}")
+            trajectory_data = read_and_concatenate_files(folder_path)
 
-        trajectory_data = read_and_concatenate_files(folder_path)
+            # 按时间戳和车辆ID排序， trajectory_data是600帧的全部轨迹
+            trajectory_data = trajectory_data.sort_values(by=['timestamp', 'vehicle_id'])
+            grouped_vehicle = trajectory_data.groupby('vehicle_id')
+            grouped_timestamp = trajectory_data.groupby('timestamp')
 
-        # 按时间戳和车辆ID排序， trajectory_data是600帧的全部轨迹
-        trajectory_data = trajectory_data.sort_values(by=['timestamp', 'vehicle_id'])
-        grouped_vehicle = trajectory_data.groupby('vehicle_id')
-        grouped_timestamp = trajectory_data.groupby('timestamp')
+            # 顺序选择目标车辆，并按照时间戳分组，每个时间戳下的others车辆按距离筛选
+            for vehicle_id, group in tqdm(grouped_vehicle):
+                target_group = group
+                target_others_info_df = filter_nearby_vehicles(vehicle_id, target_group, grouped_timestamp)
+                processed_data = process_vehicle_data(vehicle_id, target_group, target_others_info_df, all_timestamps)
+                sliced_data = slice_data_frames(processed_data)
+                traj_data = construct_features_and_labels(sliced_data, traj_data)
 
-        # 顺序选择目标车辆，并按照时间戳分组，每个时间戳下的others车辆按距离筛选
-        for vehicle_id, group in tqdm(grouped_vehicle):
-            target_vehicle = group.iloc[0]  # 获取每组的第一个车辆作为目标车辆
-            target_others_info_df = filter_nearby_vehicles(target_vehicle, grouped_timestamp)
-            processed_data = process_vehicle_data(vehicle_id, target_others_info_df, all_timestamps)
-            sliced_data = slice_data_frames(processed_data)
-            traj_data = construct_features_and_labels(sliced_data, traj_data)
-
-
-# traj_data存储的是所有样本，每个样本traj_data[i] = [feature, label]
-traj_data_save_path = 'trajectories.pkl'
-with open(traj_data_save_path, 'wb') as f:
-    pickle.dump(traj_data, f)
+    # traj_data存储的是所有样本，每个样本traj_data[i] = [feature, label]
+    traj_data_save_path = 'trajectories.pkl'
+    with open(traj_data_save_path, 'wb') as f:
+        pickle.dump(traj_data, f)
 
 
